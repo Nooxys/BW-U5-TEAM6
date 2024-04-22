9f91fb9c56d716e3766f729942f1b45f7c78016d
Ciro Vitiello <ciro.riccione@gmail.com>
2024-04-22 13:05:10 +0200

    created User Class + userType

--- src/main/java/Team6/BWU5TEAM6/entities/User.java
+++ src/main/java/Team6/BWU5TEAM6/entities/User.java
@@ -1,4 +1,75 @@
 package Team6.BWU5TEAM6.entities;
 
-public class User {
+import Team6.BWU5TEAM6.enums.UserType;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties({"password", "role", "authorities", "accountNonExpired", "credentialsNonExpired", "accountNonLocked", "enabled"})
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private long id;
+    private String username;
+    private String email;
+    private String password;
+    private String name;
+    private String surname;
+    @Enumerated(EnumType.STRING)
+    private UserType role;
+    private String avatar;
+
+    public User(String username, String email, String password, String name, String surname) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.role = UserType.USER;
+        setTemporaryAvatar();
+    }
+
+    public void setTemporaryAvatar() {
+        this.avatar = "https://ui-avatars.com/api/?name=" + this.name + "+" + this.surname;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ src/main/java/Team6/BWU5TEAM6/enums/UserType.java
@@ -0,0 +1,5 @@
+package Team6.BWU5TEAM6.enums;
+
+public enum UserType {
+    USER, ADMIN
+}
