700a4b0de5a97958718b7669f999299506d56c3e
Ciro Vitiello <ciro.riccione@gmail.com>
2024-04-22 11:46:14 +0200

    Initial commit

---
+++ src/main/java/Team6/BWU5TEAM6/BwU5Team6Application.java
@@ -0,0 +1,13 @@
+package Team6.BWU5TEAM6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwU5Team6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BwU5Team6Application.class, args);
+	}
+
+}
---
+++ src/test/java/Team6/BWU5TEAM6/BwU5Team6ApplicationTests.java
@@ -0,0 +1,13 @@
+package Team6.BWU5TEAM6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwU5Team6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
