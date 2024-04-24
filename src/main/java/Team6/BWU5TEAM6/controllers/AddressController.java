b782dba6082e0128c36279df8d1f325fba398402
MatteoOrlando <mat.orlando88@gmail.com>
2024-04-24 15:34:15 +0200

    Update AddressController.java

--- src/main/java/Team6/BWU5TEAM6/controllers/AddressController.java
+++ src/main/java/Team6/BWU5TEAM6/controllers/AddressController.java
@@ -1,4 +1,34 @@
 package Team6.BWU5TEAM6.controllers;
 
+import Team6.BWU5TEAM6.entities.Address;
+import Team6.BWU5TEAM6.services.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/addresses")
 public class AddressController {
+
+    @Autowired
+    private AddressService addressService;
+
+    @GetMapping
+    public ResponseEntity<List<Address>> getAllAddresses() {
+        List<Address> addresses = addressService.getAllAddresses();
+        return new ResponseEntity<>(addresses, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Address> getAddressById(@PathVariable("id") Long id) {
+        Address address = addressService.getAddressById(id);
+        return new ResponseEntity<>(address, HttpStatus.OK);
+    }
+
 }
